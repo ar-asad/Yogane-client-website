684ac5017e6d626efc28320a12b12f22e5a0f6a9
arasad1 <asaduzzamanaj07@gmail.com>
2023-06-08 11:22:54 +0600

    hide firebase secret key

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes/Routes';
+import AuthProvider from './context/AuthProvider';
 import {
   QueryClient,
   QueryClientProvider,
@@ -12,10 +13,14 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <div className='max-w-screen-xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </QueryClientProvider>
+    <AuthProvider>
+      {/* <HelmetProvider> */}
+      <QueryClientProvider client={queryClient}>
+        <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </QueryClientProvider>
+      {/* </HelmetProvider> */}
+    </AuthProvider>
   </React.StrictMode>,
 )
